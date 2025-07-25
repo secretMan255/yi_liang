869cacd8cfdfdf427ae785dbb2fc8ddf5b2394d7
yiliang <yapyiliangww321@gmail.com>
2025-07-25 12:24:52 +0800

    rollback

--- app/page.tsx
+++ app/page.tsx
@@ -101,7 +101,7 @@ export default function Portfolio() {
       title: "Metel Hardware Company Landing Page",
       description:
         "A landing page introduce a metel hardware company",
-      tech: ["VueJs", "Typescript", "NodeJs", "Vuetify", "TEST"],
+      tech: ["VueJs", "Typescript", "NodeJs", "Vuetify"],
       github: "https://github.com/secretMan255/Hardware-portal"
     },
     {
